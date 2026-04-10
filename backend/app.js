2aee676bcf091171b670051f662617528259c553
Rain Veskus <veskus@proton.me>
2026-04-10 22:19:16 +0300

    fixin stuff

--- backend/app.js
+++ backend/app.js
@@ -15,6 +15,8 @@ app.use(middleware.requestLogger)
 app.use(middleware.tokenExtractor)
 app.use(express.static('dist'))
 
+
+
 app.use('/api/blogs', blogsRouter)
 app.use('/api/login', loginRouter)
 app.use('/api/users', usersRouter)

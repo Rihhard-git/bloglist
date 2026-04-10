aa746be7bab792b7eb549f8b7cbc1a81a0f0ea7b
Rihhard-git <veskus@proton.me>
2026-04-11 01:02:10 +0300

    last exercise

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ const app = require('./app')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 
+// This change is for the 22. exercise.
+
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
 })
